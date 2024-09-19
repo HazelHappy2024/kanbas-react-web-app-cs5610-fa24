c6ee9dc87d368c40a14a358685768763ecbfa7be
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-09-18 23:32:54 -0700

    adddashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import {HashRouter, Link, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <h1>Welcome to Web Development!!!</h1>
+    
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/Labs/*" element={<Labs/>} />
+          <Route path ="Kanbas/*" element={<Kanbas />} />             
+        </Routes>
+      </div>
+    </HashRouter>       
   );
 }
 
