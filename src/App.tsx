d552ac4fea3833cf7bfba33950bce25745e54746
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-09-19 14:28:32 -0700

    enableindex.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
 // import logo from './logo.svg';
 //import './App.css';
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
 import {HashRouter, Route, Routes} from "react-router-dom";
+import React from 'react';
 
 function App() {
   return (
