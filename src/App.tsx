910d76f3ce1de80afdd5f37e7e7ddbc43ade0034
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-09-19 17:05:23 -0700

    editapptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-
-// import logo from './logo.svg';
-//import './App.css';
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
-import {HashRouter, Route, Routes} from "react-router-dom";
-import React from 'react';
-
-function App() {
-  return (
-    
-    <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/Labs/*" element={<Labs/>} />
-          <Route path ="Kanbas/*" element={<Kanbas />} />             
-        </Routes>
-      </div>
-    </HashRouter>       
-  );
-}
-
-export default App;
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+export default function App() {
+ return (
+  <HashRouter>
+   <div>
+    <Routes>
+     <Route path="/" element={<Navigate to="Kanbas"/>}/>
+     <Route path="/Labs/*" element={<Labs />} />
+     <Route path="/Kanbas/*" element={<Kanbas />} />
+    </Routes>
+   </div>
+  </HashRouter>
+);}
