722f965aff1ed3baf79bba14a38ee1fd36d7cc7f
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-09-30 18:48:09 -0700

    addLab2

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,16 +1,26 @@
 import Modules from "../Modules/ index";
 import CourseStatus from "./Status";
+import { Link } from "react-router-dom";  // 新增导入Link
+
 export default function Home() {
   return (
-    <table id="wd-home">
-      <tr>
-        <td valign="top">
-          <Modules />
-        </td>
-        <td valign="top">
-          <CourseStatus />
-        </td>
-      </tr>
-    </table>
+    <div id="wd-home">
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top">
+              <Modules />
+            </td>
+            <td valign="top">
+              <CourseStatus />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      
+      
+      <Link id="wd-back-to-landing" to="/Labs">Back to Landing Page</Link>
+    </div>
   );
 }
+
