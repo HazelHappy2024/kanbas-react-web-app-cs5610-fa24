01a5c4be114b1162242a21bb1d27d472a31c4c0b
HazelHappy2024 <liu.shuru@northeastern.edu>
2024-10-31 01:25:25 -0700

    implementaddcourses

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,16 +1,47 @@
 import { Link } from "react-router-dom";
 import * as db from "./Database";
+import React, { useState } from "react";
 
 export default function Dashboard() {
-  const courses = db.courses;
+
+  const [courses, setCourses] = useState<any[]>(db.courses);
+
+  
+
+  const course: any = {
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "/images/reactjs.jpg", description: "New Description"
+  };
+  const addNewCourse = () => {
+    const newCourse = { ...course,
+                        _id: new Date().getTime().toString() };
+    setCourses([...courses, { ...course, ...newCourse }]);
+  };
+
+
   return (
-    <div id="wd-dashboard">
+    <div className="p-4" id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+
+      <h5>New Course
+          <button className="btn btn-primary float-end"
+                  id="wd-add-new-course-click"
+                  onClick={addNewCourse} > Add </button>
+      </h5><br />
+      <input defaultValue={course.name} className="form-control mb-2" 
+            onChange={(e) => setCourses({ ...course, name: e.target.value }) } />
+
+      <textarea defaultValue={course.description} className="form-control"
+            onChange={(e) => setCourses({ ...course, name: e.target.value }) } />
+
+      <hr />
+
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
-      <div id="wd-dashboard-courses" className="row">
+      <div className="row" id="wd-dashboard-courses">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
-            <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div key={course._id} className="col" style={{ width: "300px" }}>
               <div className="card rounded-3 overflow-hidden">
                 <Link
                   to={`/Kanbas/Courses/${course._id}/Home`}
